2a590bc9eddfef45dfbe840bafd751f8b35ef362
Kundan <kundan kamble>
2022-06-22 01:22:23 -0700

    [KUNDAN] Added emp parttime program

--- EmpPresentAndAbsent.js
+++
@@ -1,9 +0,0 @@
-const IS_ABSENT=0
-let empcheck=Math.floor(Math.random() * 10) % 2
-if (empcheck==IS_ABSENT)
-{
-    console.log("Employee is absent")
-}
-else{
-    console.log("Employee is Present")
-}
